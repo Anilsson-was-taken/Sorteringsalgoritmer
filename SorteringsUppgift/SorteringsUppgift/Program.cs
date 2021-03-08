3590d315cc8951ce801676783f39b603c5964ef0
Alexander Nilsson <Anilsson2003@hotmail.com>
2021-03-08 17:16:23 +0100

    Quicksort

--- SorteringsUppgift/SorteringsUppgift/Program.cs
+++ SorteringsUppgift/SorteringsUppgift/Program.cs
@@ -16,80 +16,58 @@ namespace SorteringsUppgift
         {
             List<int> unsorted = new List<int>();
             List<int> sorted;
+
             Random random = new Random();
 
-            for (int i = 0; i < 100000; i++)
+            for (int i = 0; i < 40000; i++)
             {
                 unsorted.Add(random.Next(0, 1000000));
             }
-
             Stopwatch stopwatch = new Stopwatch();
             stopwatch.Start();
-            sorted = MergeSort(unsorted);
+            sorted = QuickSort(unsorted);
 
             stopwatch.Stop();
             TimeSpan ts = stopwatch.Elapsed;
             string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
             ts.Hours, ts.Minutes, ts.Seconds,
             ts.Milliseconds / 10);
-            Console.WriteLine("Listan är sorterad" + "\nTiden var " + elapsedTime);
+
+            //foreach (int x in sorted)
+            //{
+                //Console.Write(x + " ");
+            //}
+
+                Console.WriteLine("Listan är sorterad" + "\nTiden var " + elapsedTime);
         }
 
-        private static List<int> MergeSort(List<int> unsorted)
+        static List<int> QuickSort(List<int> lst)
         {
-            if (unsorted.Count <= 1)
-                return unsorted;
+            if (lst.Count <= 1)
+                return lst;
+            int pivotIndex = lst.Count / 2;
+            int pivot = lst[pivotIndex];
             List<int> left = new List<int>();
             List<int> right = new List<int>();
 
-            int middle = unsorted.Count / 2;
-            for (int i = 0; i < middle; i++)
+            for (int i = 0; i < lst.Count; i++)
             {
-                left.Add(unsorted[i]);
-            }
-            for (int i = middle; i < unsorted.Count; i++)
-            {
-                right.Add(unsorted[i]);
-            }
-            left = MergeSort(left);
-            right = MergeSort(right);
-            return Merge(left, right);
-        }
+                if (i == pivotIndex) continue;
 
-        private static List<int> Merge(List<int> left, List<int> right)
-        {
-            List<int> result = new List<int>();
-            while (left.Count > 0 || right.Count > 0)
-            {
-                if (left.Count > 0 && right.Count > 0)
+                if (lst[i] <= pivot)
                 {
-                    if (left.First() <= right.First())
-                    {
-                        result.Add(left.First());
-                        left.Remove(left.First());
-                    }
-                    else
-                    {
-                        result.Add(right.First());
-                        right.Remove(right.First());
-                    }
+                    left.Add(lst[i]);
                 }
-                else if (left.Count > 0)
+                else
                 {
-                    result.Add(left.First());
-                    left.Remove(left.First());
-                }
-                else if (right.Count > 0)
-                {
-                    result.Add(right.First());
-
-                    right.Remove(right.First());
+                    right.Add(lst[i]);
                 }
             }
-            return result;
+
+            List<int> sorted = QuickSort(left);
+            sorted.Add(pivot);
+            sorted.AddRange(QuickSort(right));
+            return sorted;
         }
-        //Lyckades inte med att få in gamla metoden myList in i mergesort pga array problem
-        //försökte konvertera myList till array men misslyckades 
-        //så jag valde att göra en lista i taget genom att ändra rad 21 vilket fungerade
     }
 }
